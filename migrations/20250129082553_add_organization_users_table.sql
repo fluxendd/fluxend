eae900c8f0e9d5043b5b6a984df5309f8df537e9
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-01-29 15:07:14 +0100

    add cascade for org users

--- migrations/20250129082553_add_organization_users_table.sql
+++ migrations/20250129082553_add_organization_users_table.sql
@@ -8,7 +8,7 @@ CREATE TABLE organization_users (
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-ALTER TABLE organization_users ADD CONSTRAINT fk_organization_users_organizations FOREIGN KEY (organization_id) REFERENCES organizations (id);
+ALTER TABLE organization_users ADD CONSTRAINT fk_organization_users_organizations FOREIGN KEY (organization_id) REFERENCES organizations (id) ON DELETE CASCADE;
 ALTER TABLE organization_users ADD CONSTRAINT fk_organization_users_users FOREIGN KEY (user_id) REFERENCES users (id);
 -- +goose StatementEnd
 
