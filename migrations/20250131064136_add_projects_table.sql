63902c160b514c0e63416189458cca1b486d78d2
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-01-31 07:47:02 +0100

    add more tables

---
+++ migrations/20250131064136_add_projects_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY,
+    organization_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    db_name uuid NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE projects ADD CONSTRAINT fk_organization_id FOREIGN KEY (organization_id) REFERENCES organizations(id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE projects;
+-- +goose StatementEnd
---
+++ migrations/20250131064151_add_tables_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE tables (
+    id SERIAL PRIMARY KEY,
+    project_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    fields JSONB NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE tables ADD CONSTRAINT fk_project_id FOREIGN KEY (project_id) REFERENCES projects(id);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE tables;
+-- +goose StatementEnd
