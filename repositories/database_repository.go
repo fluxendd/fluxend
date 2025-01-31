40d27bdd3e9941c06176c6896fb237af99f8561a
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-01-31 11:35:05 +0100

    more methods in database_repository.go

--- repositories/database_repository.go
+++ repositories/database_repository.go
@@ -19,16 +19,55 @@ func NewDatabaseRepository(injector *do.Injector) (*DatabaseRepository, error) {
 func (r *DatabaseRepository) Create(name string) error {
 	_, err := r.db.Exec(fmt.Sprintf("CREATE DATABASE %s", name))
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
 
 	return nil
 }
 
-// Drop
-// Recreate
-// List
-// GetSchema
-// Exists
-// Connect
+func (r *DatabaseRepository) DropIfExists(name string) error {
+	_, err := r.db.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", name))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *DatabaseRepository) Recreate(name string) error {
+	err := r.DropIfExists(name)
+	if err != nil {
+		return err
+	}
+
+	err = r.Create(name)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *DatabaseRepository) List() ([]string, error) {
+	var databases []string
+	err := r.db.Select(&databases, "SELECT datname FROM pg_database WHERE datistemplate = false")
+	if err != nil {
+		return []string{}, err
+	}
+
+	return databases, nil
+}
+
+func (r *DatabaseRepository) Exists(name string) (bool, error) {
+	var count int
+	err := r.db.Get(&count, "SELECT COUNT(*) FROM pg_database WHERE datname = $1", name)
+	if err != nil {
+		return false, err
+	}
+
+	return count > 0, nil
+}
+
+func (r *DatabaseRepository) Connect(name string) (*sqlx.DB, error) {
+	return sqlx.Connect("postgres", fmt.Sprintf("dbname=%s", name))
+}
