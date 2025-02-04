a6d7373c6a04c768952fbed71ceae4b9824da47f
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-02-04 08:35:55 +0100

    finish row repo

--- repositories/client_row_repository.go
+++ repositories/client_row_repository.go
@@ -2,6 +2,7 @@ package repositories
 
 import (
 	"fluxton/models"
+	"fluxton/utils"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"strings"
@@ -14,6 +15,64 @@ type ClientRowRepository struct {
 // NewClientRowRepository A repository for dynamically inserting rows into dynamic tables
 func NewClientRowRepository(connection *sqlx.DB) (*ClientRowRepository, error) {
 	return &ClientRowRepository{connection: connection}, nil
+}
+
+func (r *CoreTableRepository) List(tableName string, paginationParams utils.PaginationParams) ([]models.Row, error) {
+	offset := (paginationParams.Page - 1) * paginationParams.Limit
+
+	query := `
+		SELECT * FROM %s
+		ORDER BY 
+			:sort DESC
+		LIMIT 
+			:limit 
+		OFFSET 
+			:offset;
+
+	`
+
+	query = fmt.Sprintf(query, tableName)
+
+	params := map[string]interface{}{
+		"sort":   paginationParams.Sort,
+		"limit":  paginationParams.Limit,
+		"offset": offset,
+	}
+
+	rows, err := r.db.NamedQuery(query, params)
+	if err != nil {
+		return nil, fmt.Errorf("could not retrieve rows: %v", err)
+	}
+	defer rows.Close()
+
+	var results []models.Row
+	for rows.Next() {
+		var result models.Row
+		if err := rows.StructScan(&result); err != nil {
+			return nil, fmt.Errorf("could not scan row: %v", err)
+		}
+		results = append(results, result)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("could not iterate over rows: %v", err)
+	}
+
+	return results, nil
+}
+
+func (r *ClientRowRepository) GetByID(tableName string, rowID uint64) (models.Row, error) {
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id = $1", tableName)
+
+	resultRow := make(models.Row)
+	row := r.connection.QueryRowx(query, rowID)
+
+	err := row.MapScan(resultRow)
+	if err != nil {
+		return nil, fmt.Errorf("error getting row: %v", err)
+	}
+
+	return resultRow, nil
 }
 
 func (r *ClientRowRepository) Create(tableName string, fields models.Row) (uint64, error) {
@@ -49,16 +108,44 @@ func (r *ClientRowRepository) Create(tableName string, fields models.Row) (uint6
 	return insertedID, nil
 }
 
-func (r *ClientRowRepository) GetByID(tableName string, id uint64) (models.Row, error) {
-	query := fmt.Sprintf("SELECT * FROM %s WHERE id = $1", tableName)
-
-	resultRow := make(models.Row)
-	row := r.connection.QueryRowx(query, id)
-
-	err := row.MapScan(resultRow)
-	if err != nil {
-		return nil, fmt.Errorf("error getting row: %v", err)
+func (r *ClientRowRepository) Update(tableName string, rowID uint64, fields models.Row) error {
+	if len(fields) == 0 {
+		return fmt.Errorf("no fields to update")
 	}
 
-	return resultRow, nil
+	var columns []string
+	var values []interface{}
+
+	i := 1
+	for col, val := range fields {
+		columns = append(columns, fmt.Sprintf("%s = $%d", col, i))
+		values = append(values, val)
+		i++
+	}
+
+	query := fmt.Sprintf(
+		"UPDATE %s SET %s WHERE id = $%d",
+		tableName,
+		strings.Join(columns, ", "),
+		i,
+	)
+	values = append(values, rowID)
+
+	_, err := r.connection.Exec(query, values...)
+	if err != nil {
+		return fmt.Errorf("error updating row: %v", err)
+	}
+
+	return nil
+}
+
+func (r *ClientRowRepository) Delete(tableName string, rowID uint64) error {
+	query := fmt.Sprintf("DELETE FROM %s WHERE id = $1", tableName)
+
+	_, err := r.connection.Exec(query, rowID)
+	if err != nil {
+		return fmt.Errorf("error deleting row: %v", err)
+	}
+
+	return nil
 }
