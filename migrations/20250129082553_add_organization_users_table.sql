3fa739ae867f73d2cf9f93cbdd86756174014bef
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-02-07 09:17:03 +0100

    simplify sql statements

--- migrations/20250129082553_add_organization_users_table.sql
+++ migrations/20250129082553_add_organization_users_table.sql
@@ -2,14 +2,11 @@
 -- +goose StatementBegin
 CREATE TABLE fluxton.organization_users (
     id SERIAL PRIMARY KEY,
-    organization_id UUID NOT NULL,
-    user_id UUID NOT NULL,
+    organization_id UUID NOT NULL REFERENCES fluxton.organizations (id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES authentication.users (id),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-ALTER TABLE fluxton.organization_users ADD CONSTRAINT fk_organization_users_organizations FOREIGN KEY (organization_id) REFERENCES fluxton.organizations (id) ON DELETE CASCADE;
-ALTER TABLE fluxton.organization_users ADD CONSTRAINT fk_organization_users_users FOREIGN KEY (user_id) REFERENCES authentication.users (id);
 -- +goose StatementEnd
 
 -- +goose Down
