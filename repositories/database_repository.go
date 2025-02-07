b312d7356bbe98d73391017b7e6b14f645f98f52
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-02-07 10:35:14 +0100

    seed default sql files on client db creation

--- repositories/database_repository.go
+++ repositories/database_repository.go
@@ -3,7 +3,11 @@ package repositories
 import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/gommon/log"
 	"github.com/samber/do"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type DatabaseRepository struct {
@@ -22,7 +26,7 @@ func (r *DatabaseRepository) Create(name string) error {
 		return err
 	}
 
-	return nil
+	return r.importSeedFiles(name)
 }
 
 func (r *DatabaseRepository) DropIfExists(name string) error {
@@ -70,4 +74,48 @@ func (r *DatabaseRepository) Exists(name string) (bool, error) {
 
 func (r *DatabaseRepository) Connect(name string) (*sqlx.DB, error) {
 	return sqlx.Connect("postgres", fmt.Sprintf("dbname=%s sslmode=disable", name))
+}
+
+func (r *DatabaseRepository) importSeedFiles(databaseName string) error {
+	connection, err := r.Connect(databaseName)
+	defer connection.Close()
+
+	if err != nil {
+		return fmt.Errorf("could not connect to database: %v", err)
+	}
+
+	seedDir := "seeders/client"
+
+	// Read all files in the directory
+	files, err := os.ReadDir(seedDir)
+	if err != nil {
+		return fmt.Errorf("could not read seed directory: %v", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".sql") {
+			continue
+		}
+
+		filePath := filepath.Join(seedDir, file.Name())
+
+		// Load the contents of the SQL file
+		sqlContent, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Debug("DB: %s => Skipping file %s: could not read file: %v", databaseName, filePath, err)
+
+			continue
+		}
+
+		// Execute the SQL statements
+		if _, err := connection.Exec(string(sqlContent)); err != nil {
+			log.Debug("DB: %s => Skipping file %s: could not execute SQL: %v", databaseName, filePath, err)
+
+			continue
+		}
+
+		log.Debug("DB: %s => Successfully executed seed file %s", databaseName, filePath)
+	}
+
+	return nil
 }
