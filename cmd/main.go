04363aee7a48a87b947c655ec9aa1dbcd043f64a
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-02-08 09:50:11 +0100

    print registered routes

--- cmd/main.go
+++ cmd/main.go
@@ -6,49 +6,66 @@ import (
 	"fluxton/di"
 	"fluxton/routes"
 	"fluxton/seeders"
+	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"github.com/samber/do"
+	"strings"
 )
 
 func main() {
-	// Define the flag for choosing between "server" and "seed"
-	mode := flag.String("cmd", "server", "Possible values: 'server', 'seed'")
+	mode := flag.String("cmd", "server", "Possible values: 'server', 'seed', 'routes'")
 	flag.Parse()
 
-	// Load environment variables
-	err := godotenv.Load()
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	// Initialize the DI container
 	container := di.InitializeContainer()
+	handleMode(container, *mode)
+}
 
-	if *mode == "seed" {
-		// Run the seeders if the mode is "seed"
+// handleMode determines which functionality to execute based on the "cmd" flag
+func handleMode(container *do.Injector, mode string) {
+	switch mode {
+	case "seed":
 		runSeeders(container)
-	} else {
-		// Otherwise, start the server
+	case "routes":
+		PrintRoutes(container)
+	default:
 		startServer(container)
 	}
 }
 
 func startServer(container *do.Injector) {
+	e := setupServer(container)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// setupServer sets up the Echo server with controllers, routes, and middleware
+func setupServer(container *do.Injector) *echo.Echo {
 	e := echo.New()
 
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// Register routes
+	registerRoutes(e, container)
+
+	return e
+}
+
+// registerRoutes registers all routes in the Echo instance
+func registerRoutes(e *echo.Echo, container *do.Injector) {
 	// Controllers
 	userController := do.MustInvoke[*controllers.UserController](container)
 	organizationController := do.MustInvoke[*controllers.OrganizationController](container)
 	organizationUserController := do.MustInvoke[*controllers.OrganizationUserController](container)
 	projectController := do.MustInvoke[*controllers.ProjectController](container)
-
 	tableController := do.MustInvoke[*controllers.TableController](container)
 	columnController := do.MustInvoke[*controllers.ColumnController](container)
 	rowController := do.MustInvoke[*controllers.RowController](container)
@@ -60,11 +77,9 @@ func startServer(container *do.Injector) {
 	routes.RegisterProjectRoutes(e, projectController)
 	routes.RegisterTableRoutes(e, tableController, columnController, indexController)
 	routes.RegisterRowRoutes(e, rowController)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":8080"))
 }
 
+// runSeeders runs all seeders defined in the seeders package
 func runSeeders(container *do.Injector) {
 	log.Info("Starting database seeding...")
 
@@ -73,9 +88,53 @@ func runSeeders(container *do.Injector) {
 		seeders.SeedUsers,
 	}
 
+	// Execute each seeder
 	for _, seeder := range seedersToRun {
 		seeder(container)
 	}
 
 	log.Info("Database seeding completed successfully.")
+}
+
+func PrintRoutes(container *do.Injector) {
+	e := setupServer(container)
+
+	registeredRoutes := e.Routes()
+
+	// Map to hold groups of routes based on their prefix
+	routesGrouped := make(map[string][]string)
+
+	// Iterate through the routes and group them by prefix
+	for _, route := range registeredRoutes {
+		if route.Method == "echo_route_not_found" {
+			continue
+		}
+
+		// Extract the prefix (e.g., /api/projects from /api/projects/:projectID/tables)
+		routePrefix := getRoutePrefix(route.Path)
+
+		// Group the routes by prefix
+		routesGrouped[routePrefix] = append(routesGrouped[routePrefix], fmt.Sprintf("Method: %-6s  Path: %-30s", route.Method, route.Path))
+	}
+
+	// Print the grouped routes
+	for prefix, matchedRoutes := range routesGrouped {
+		fmt.Printf("\nRoutes for %s:\n", prefix)
+
+		for _, route := range matchedRoutes {
+			fmt.Println(route)
+		}
+	}
+}
+
+func getRoutePrefix(path string) string {
+	// Split the path by '/' and take the first two segments (or fewer if the path is shorter)
+	segments := strings.Split(path, "/")
+
+	// Return the prefix by joining the first segments (e.g., /api/projects)
+	if len(segments) > 1 {
+		return "/" + segments[2]
+	}
+
+	return path
 }
