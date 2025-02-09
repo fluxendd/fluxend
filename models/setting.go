1a702788c8e9f00b97149866be4518d5f7617eeb
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-02-09 18:16:36 +0100

    add setting model

---
+++ models/setting.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+)
+
+type Setting struct {
+	ID           int       `db:"id"`
+	Name         string    `db:"name"`
+	Value        string    `db:"value"`
+	DefaultValue string    `db:"default_value"`
+	CreatedAt    time.Time `db:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at"`
+}
+
+func (s Setting) GetTableName() string {
+	return "settings"
+}
