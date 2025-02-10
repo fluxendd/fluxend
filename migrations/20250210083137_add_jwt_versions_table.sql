f2d69fa545f826a68276119968f3aaab344e838d
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-02-10 16:42:37 +0100

    add jwt_versions table

---
+++ migrations/20250210083137_add_jwt_versions_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE authentication.jwt_versions (
+    id SERIAL PRIMARY KEY,
+    user_id UUID NOT NULL UNIQUE REFERENCES authentication.users(id) ON DELETE CASCADE,
+    version INT NOT NULL DEFAULT 0,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE authentication.jwt_versions;
+-- +goose StatementEnd
