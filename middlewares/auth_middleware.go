9412810e904adfb232cf194503ef1c8a1a8656e6
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-02-10 16:43:14 +0100

    validate user token in auth_middleware.go

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"fluxton/errs"
 	"fluxton/models"
+	"fluxton/repositories"
 	"fluxton/responses"
 	"fmt"
 	"github.com/golang-jwt/jwt/v5"
@@ -12,49 +13,61 @@ import (
 	"strings"
 )
 
-func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		authHeader := c.Request().Header.Get("Authorization")
-		if authHeader == "" {
-			return responses.UnauthorizedResponse(c, "auth.error.tokenRequired")
-		}
-
-		// Token usually comes in the format "Bearer <token>"
-		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-		if tokenString == authHeader { // If the token doesn't start with "Bearer "
-			return responses.UnauthorizedResponse(c, "auth.error.tokenInvalid")
-		}
-
-		// Parse the token
-		claims := jwt.MapClaims{}
-		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-			// Check the signing method
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+func AuthMiddleware(userRepo *repositories.UserRepository) echo.MiddlewareFunc {
+	// Outer function accepts the next handler
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		// Inner function executes for each request
+		return func(c echo.Context) error {
+			authHeader := c.Request().Header.Get("Authorization")
+			if authHeader == "" {
+				return responses.UnauthorizedResponse(c, "auth.error.tokenRequired")
 			}
 
-			// Return the JWT_SECRET as the key
-			return []byte(os.Getenv("JWT_SECRET")), nil
-		})
+			// Token usually comes in the format "Bearer <token>"
+			tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+			if tokenString == authHeader { // If the token doesn't start with "Bearer "
+				return responses.UnauthorizedResponse(c, "auth.error.tokenInvalid")
+			}
 
-		if err != nil || !token.Valid {
-			// Token is invalid or expired
-			return responses.ErrorResponse(c, errs.NewUnauthorizedError("auth.error.tokenInvalid"))
+			// Parse the token
+			claims := jwt.MapClaims{}
+			token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+				// Check the signing method
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+				}
+
+				// Return the JWT_SECRET as the key
+				return []byte(os.Getenv("JWT_SECRET")), nil
+			})
+
+			if err != nil || !token.Valid {
+				// Token is invalid or expired
+				return responses.ErrorResponse(c, errs.NewUnauthorizedError("auth.error.tokenInvalid"))
+			}
+
+			userUUID, err := uuid.Parse(claims["id"].(string))
+			if err != nil {
+				return responses.ErrorResponse(c, errs.NewUnauthorizedError("auth.error.tokenInvalid"))
+			}
+
+			currentJWTVersion := int(claims["version"].(float64))
+			existingJWTVersion, err := userRepo.GetJWTVersion(userUUID)
+			if err != nil {
+				return responses.ErrorResponse(c, err)
+			}
+
+			if currentJWTVersion != existingJWTVersion {
+				return responses.UnauthorizedResponse(c, "auth.error.tokenInvalid")
+			}
+
+			c.Set("user", models.AuthUser{
+				ID:     userUUID,
+				RoleID: int(claims["role_id"].(float64)),
+			})
+
+			// Proceed to the next handler if everything is valid
+			return next(c)
 		}
-
-		// Optionally: You can store the user data from the token for later use in the request context
-		// For example, store user ID or role in context for further authorization checks.
-		userUUID, err := uuid.Parse(claims["id"].(string))
-		if err != nil {
-			return responses.ErrorResponse(c, errs.NewUnauthorizedError("auth.error.tokenInvalid"))
-		}
-
-		c.Set("user", models.AuthUser{
-			ID:     userUUID,
-			RoleID: int(claims["role_id"].(float64)),
-		})
-
-		// Proceed to the next handler if everything is valid
-		return next(c)
 	}
 }
