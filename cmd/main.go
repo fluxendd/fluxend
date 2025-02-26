5f8a834e2611acb62d5438e617a2a29f39162580
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-02-26 14:26:57 +0100

    simplify file structure

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fluxton/controllers"
-	"fluxton/di"
 	"fluxton/middlewares"
 	"fluxton/repositories"
 	"fluxton/routes"
@@ -25,7 +24,7 @@ func main() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	container := di.InitializeContainer()
+	container := InitializeContainer()
 	handleMode(container, *mode)
 }
 
