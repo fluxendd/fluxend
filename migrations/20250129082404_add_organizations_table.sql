36e97bae95fcf2296a8bf40b280d646eb98ad20e
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-02-26 08:33:31 +0100

    revise migrations

--- migrations/20250129082404_add_organizations_table.sql
+++ migrations/20250129082404_add_organizations_table.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE fluxton.organizations (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
-    created_by UUID NOT NULL REFERENCES authentication.users(id),
-    updated_by UUID NOT NULL REFERENCES authentication.users(id),
+    created_by UUID NOT NULL REFERENCES authentication.users(uuid),
+    updated_by UUID NOT NULL REFERENCES authentication.users(uuid),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
