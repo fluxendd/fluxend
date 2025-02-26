4766a1a9c52eb48113ad0daf49e15210f28e1db5
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-02-26 08:38:32 +0100

    add forms table

---
+++ migrations/20250226073506_add_fluxton_forms_table.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE fluxton.forms (
+   uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+   project_uuid UUID NOT NULL,
+   name VARCHAR(255) NOT NULL,
+   description TEXT NULL,
+   created_by UUID NOT NULL,
+   updated_by UUID NULL,
+   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+   updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+
+   CONSTRAINT fk_forms_project FOREIGN KEY (project_uuid) REFERENCES fluxton.projects(uuid) ON DELETE CASCADE,
+   CONSTRAINT fk_forms_created_by FOREIGN KEY (created_by) REFERENCES users(uuid) ON DELETE CASCADE,
+   CONSTRAINT fk_forms_updated_by FOREIGN KEY (updated_by) REFERENCES users(uuid) ON DELETE SET NULL
+);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS fluxton.forms CASCADE;
+-- +goose StatementEnd
