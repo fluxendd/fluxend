ac25454e5806d01ac0a90f7880ad25fb7768888b
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-02-26 08:49:06 +0100

    fix migration issues

--- migrations/20250131064145_add_project_settings_table.sql
+++ migrations/20250131064145_add_project_settings_table.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 CREATE TABLE fluxton.project_settings (
     id SERIAL PRIMARY KEY,
-    project_id UUID NOT NULL REFERENCES fluxton.projects (id) ON DELETE CASCADE,
+    project_id UUID NOT NULL REFERENCES fluxton.projects (uuid) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     value TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
