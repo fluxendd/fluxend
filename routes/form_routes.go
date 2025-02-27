c259da4ca1cd00a3b50a35de2d82d7c40ea73677
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-02-27 16:42:06 +0100

    FLXX-2: add form routes

---
+++ routes/form_routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"fluxton/controllers"
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterFormRoutes(e *echo.Echo, authMiddleware echo.MiddlewareFunc, FormController *controllers.FormController) {
+	projectsGroup := e.Group("api/projects/:projectID/forms", authMiddleware)
+
+	projectsGroup.POST("", FormController.Store)
+	projectsGroup.GET("", FormController.List)
+	projectsGroup.GET("/:formUUID", FormController.Show)
+	projectsGroup.PUT("/:formUUID", FormController.Update)
+	projectsGroup.DELETE("/:formUUID", FormController.Delete)
+}
