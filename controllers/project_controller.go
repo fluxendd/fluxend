ed2154dc62ccc8243d2c15f5e50280dfcdafd372
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-03 05:28:00 +0100

    FLXX-88: add swagger docs project endpoints

--- controllers/project_controller.go
+++ controllers/project_controller.go
@@ -20,6 +20,24 @@ func NewProjectController(injector *do.Injector) (*ProjectController, error) {
 	return &ProjectController{projectService: projectService}, nil
 }
 
+// List all projects
+//
+// @Summary List all projects
+// @Description Get all projects for a specific organization
+// @Tags projects
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param organizationUUID query string true "Organization UUID"
+//
+// @Success 200 {object} responses.Response{content=[]resources.ProjectResponse} "List of projects"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects [get]
 func (pc *ProjectController) List(c echo.Context) error {
 	authUser, _ := utils.NewAuth(c).User()
 
@@ -37,6 +55,25 @@ func (pc *ProjectController) List(c echo.Context) error {
 	return responses.SuccessResponse(c, resources.ProjectResourceCollection(projects))
 }
 
+// Show details of a single project
+//
+// @Summary Show details of a single project
+// @Description Get details of a specific project
+// @Tags projects
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param projectID path string true "Project ID"
+//
+// @Success 200 {object} responses.Response{content=resources.ProjectResponse} "Project details"
+// @Failure 422 "Unprocessable entity"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects/{projectID} [get]
 func (pc *ProjectController) Show(c echo.Context) error {
 	authUser, _ := utils.NewAuth(c).User()
 
@@ -53,6 +90,26 @@ func (pc *ProjectController) Show(c echo.Context) error {
 	return responses.SuccessResponse(c, resources.ProjectResource(&project))
 }
 
+// Store creates a new project
+//
+// @Summary Create a new project
+// @Description Create a new project for a specific organization
+// @Tags projects
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param organizationUUID query string true "Organization UUID"
+// @Param name body project_requests.CreateRequest true "Project name"
+//
+// @Success 201 {object} responses.Response{content=resources.ProjectResponse} "Project details"
+// @Failure 422 "Unprocessable entity"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects [post]
 func (pc *ProjectController) Store(c echo.Context) error {
 	var request project_requests.CreateRequest
 	if err := request.BindAndValidate(c); err != nil {
@@ -69,6 +126,26 @@ func (pc *ProjectController) Store(c echo.Context) error {
 	return responses.CreatedResponse(c, resources.ProjectResource(&project))
 }
 
+// Update a project
+//
+// @Summary Update a project
+// @Description Update a project for a specific organization
+// @Tags projects
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param projectID path string true "Project ID"
+// @Param name body project_requests.UpdateRequest true "Project name"
+//
+// @Success 200 {object} responses.Response{content=resources.ProjectResponse} "Project details"
+// @Failure 422 "Unprocessable entity"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects/{projectID} [put]
 func (pc *ProjectController) Update(c echo.Context) error {
 	var request project_requests.UpdateRequest
 	if err := request.BindAndValidate(c); err != nil {
@@ -90,6 +167,24 @@ func (pc *ProjectController) Update(c echo.Context) error {
 	return responses.SuccessResponse(c, resources.ProjectResource(updatedOrganization))
 }
 
+// Delete a project
+//
+// @Summary Delete a project
+// @Description Remove a project from the organization
+// @Tags projects
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param projectID path string true "Project ID"
+//
+// @Success 200 {object} responses.Response{} "Project deleted"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects/{projectID} [delete]
 func (pc *ProjectController) Delete(c echo.Context) error {
 	authUser, _ := utils.NewAuth(c).User()
 
