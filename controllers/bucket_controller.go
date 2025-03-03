3fd66f46ff9e275054aced074285946a6339870b
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-03 05:16:15 +0100

    FLXX-88: add swagger docs for bucket endpoints

--- controllers/bucket_controller.go
+++ controllers/bucket_controller.go
@@ -20,6 +20,24 @@ func NewBucketController(injector *do.Injector) (*BucketController, error) {
 	return &BucketController{bucketService: bucketService}, nil
 }
 
+// List retrieves all buckets
+//
+// @Summary List all buckets
+// @Description Retrieve a list of buckets in a specified project.
+// @Tags buckets
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"Bearer Token"
+// @Param projectUUID path string true "Project ID"
+//
+// @Success 200 {object} responses.Response{content=[]resources.BucketResponse} "List of buckets"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects/{projectUUID}/storage [get]
 func (bc *BucketController) List(c echo.Context) error {
 	authUser, _ := utils.NewAuth(c).User()
 
@@ -37,6 +55,26 @@ func (bc *BucketController) List(c echo.Context) error {
 	return responses.SuccessResponse(c, resources.BucketResourceCollection(buckets))
 }
 
+// Show retrieves details of a specific bucket.
+//
+// @Summary Show details of a single bucket
+// @Description Get details of a specific bucket
+// @Tags buckets
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param projectUUID path string true "Project UUID"
+// @Param bucketUUID path string true "Bucket UUID"
+//
+// @Success 200 {object} responses.Response{content=resources.BucketResponse} "Bucket details"
+// @Failure 422 "Unprocessable entity"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects/{projectUUID}/storage/{bucketUUID} [get]
 func (bc *BucketController) Show(c echo.Context) error {
 	authUser, _ := utils.NewAuth(c).User()
 
@@ -58,6 +96,26 @@ func (bc *BucketController) Show(c echo.Context) error {
 	return responses.SuccessResponse(c, resources.BucketResource(&bucket))
 }
 
+// Store creates a new bucket
+//
+// @Summary Create a new bucket
+// @Description Add a new bucket to a project
+// @Tags buckets
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param projectUUID path string true "Project UUID"
+// @Param bucket body bucket_requests.CreateRequest true "Bucket details"
+//
+// @Success 201 {object} responses.Response{content=resources.BucketResponse} "Bucket created"
+// @Failure 422 "Unprocessable entity"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects/{projectUUID}/storage [post]
 func (bc *BucketController) Store(c echo.Context) error {
 	var request bucket_requests.CreateRequest
 	if err := request.BindAndValidate(c); err != nil {
@@ -79,6 +137,27 @@ func (bc *BucketController) Store(c echo.Context) error {
 	return responses.CreatedResponse(c, resources.BucketResource(&bucket))
 }
 
+// Update a bucket
+//
+// @Summary Update a bucket
+// @Description Modify an existing bucket's details
+// @Tags buckets
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param projectUUID path string true "Project UUID"
+// @Param bucketUUID path string true "Bucket UUID"
+// @Param bucket body bucket_requests.CreateRequest true "Bucket details"
+//
+// @Success 200 {object} responses.Response{content=resources.BucketResponse} "Bucket updated"
+// @Failure 422 "Unprocessable entity"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects/{projectUUID}/storage/{bucketUUID} [put]
 func (bc *BucketController) Update(c echo.Context) error {
 	var request bucket_requests.CreateRequest
 	if err := request.BindAndValidate(c); err != nil {
@@ -100,6 +179,24 @@ func (bc *BucketController) Update(c echo.Context) error {
 	return responses.SuccessResponse(c, resources.BucketResource(updatedBucket))
 }
 
+// Delete a bucket
+//
+// @Summary Delete a bucket
+// @Description Remove a bucket from a project
+// @Tags buckets
+//
+// @Accept json
+// @Produce json
+//
+// @Param Authorization header string true "Bearer Token"
+// @Param bucketUUID path string true "Bucket UUID"
+//
+// @Success 204 "Bucket deleted"
+// @Failure 400 "Invalid input"
+// @Failure 401 "Unauthorized"
+// @Failure 500 "Internal server error"
+//
+// @Router /projects/{projectUUID}/storage/{bucketUUID} [delete]
 func (bc *BucketController) Delete(c echo.Context) error {
 	authUser, _ := utils.NewAuth(c).User()
 
