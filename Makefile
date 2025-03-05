a38018efc4e9889020b19fe1291e4375d298f7e6
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-03-05 16:59:29 +0100

    simplify make commands

--- Makefile
+++ Makefile
@@ -7,29 +7,23 @@ DATABASE_CONNECTION="user=${DATABASE_USER} password=${DATABASE_PASSWORD} dbname=
 include makefiles/*.mk
 
 # Commands
-.PHONY: help run build migrate
+.PHONY: help serve routes build setup up down login-app login-db postgrest-list postgrest-destroy docs
 
 help: ## Shows this help
 	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_\-\.]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
-run: ## Run the project
-	@go run cmd/*.go
-
-list_routes: ## Show the routes
-	@go run cmd/*.go -cmd=routes
-
-build: ## Build the project
-	@go build -o bin/fluxton cmd/*.go
-
-lint: ## Run linter
-	@golangci-lint run
-
-lint-fix: ## Run linter and fix
-	@golangci-lint run --fix
-
 setup: ## Setup the project
 	cp .env.example .env
 	make up
+
+serve: ## Run the project in development mode
+	@go run main.go server
+
+routes: ## Show all the available routes
+	@go run main.go routes
+
+build: ## Build the project
+	@go build -o bin/fluxton main.go
 
 up: ## Start the project
 	@docker-compose up -d
@@ -37,16 +31,16 @@ up: ## Start the project
 down: ## Stop the project
 	@docker-compose down
 
-login-app: ## Login to fluxton container
+login.app: ## Login to fluxton container
 	@docker exec -it fluxton_app /bin/sh
 
-login-db: ## Login to database container
+login.db: ## Login to database container
 	@docker exec -it fluxton_db /bin/bash
 
-postgrest-list: ## List all postgrest containers
+pgr.list: ## List all postgrest containers
 	@docker ps --filter "name=postgrest_" --format "table {{.ID}}\t{{.Names}}\t{{.Ports}}\t{{.CreatedAt}}\t{{.Status}}"
 
-postgrest-destroy: ## Destroy all postgrest containers
+pgr.destroy: ## Destroy all postgrest containers
 	@docker rm -f $(shell docker ps -a -q --filter "name=postgrest_")
 
 docs: ## Generate docs
--- makefiles/database.mk
+++ makefiles/database.mk
@@ -1,6 +1,6 @@
 .PHONY: migrate-create migrate-up migrate-down migrate-status migrate-reset migrate-redo migrate-fresh seed seed-fresh
 
-drop-user-databases: ## Drop all user-created databases
+drop.user.dbs: ## Drop all user-created databases
 	@docker exec -i fluxton_db psql -U ${DATABASE_USER} -d ${DATABASE_NAME} -t -c "SELECT datname FROM pg_database WHERE datname LIKE 'udb_%';" | sed 's/^[ \t]*//' | while read dbname; do \
 		if [ ! -z "$$dbname" ]; then \
 			echo "Dropping database: $$dbname"; \
@@ -8,35 +8,35 @@ drop-user-databases: ## Drop all user-created databases
 		fi \
 	done
 
-migrate-create: ## Create a new database migration
+migrate.create: ## Create a new database migration
 	@read -p "Enter migration name: " name; \
 	goose -dir database/migrations create $$name sql
 
-migrate-up: ## Run database migrations
+migrate.up: ## Run database migrations
 	goose -dir database/migrations postgres ${DATABASE_CONNECTION} up
 
-migrate-down: ## Rollback database migrations
+migrate.down: ## Rollback database migrations
 	goose -dir database/migrations postgres ${DATABASE_CONNECTION} down
 
-migrate-status: ## Show the status of the database migrations
+migrate.status: ## Show the status of the database migrations
 	goose -dir database/migrations postgres ${DATABASE_CONNECTION} status
 
-migrate-reset: ## Rollback all migrations and run them again
+migrate.reset: ## Rollback all migrations and run them again
 	goose -dir database/migrations postgres ${DATABASE_CONNECTION} reset
 
-migrate-redo: ## Rollback the last migration and run it again
+migrate.redo: ## Rollback the last migration and run it again
 	goose -dir database/migrations postgres ${DATABASE_CONNECTION} redo
 
-migrate-fresh: ## Rollback all migrations and run them again
-	make drop-user-databases
-	make migrate-reset
-	make migrate-up
+migrate.fresh: ## Rollback all migrations and run them again
+	make drop.user.dbs
+	make migrate.reset
+	make migrate.up
 
 seed: ## Seed the database
-	@go run cmd/*.go -cmd=seed
+	@go run main.go seed
 
-seed-fresh: ## Seed the database with fresh data
-	make migrate-fresh
+seed.fresh: ## Seed the database with fresh data
+	make migrate.fresh
 	make seed
 
 
