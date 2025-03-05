a3922473f2620e396bdf5dd6b676fcd7db305514
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-03-05 06:34:23 +0100

    FLXX-12: add form_response_repository.go

--- repositories/form_response_repository.go
+++ repositories/form_response_repository.go
@@ -1,16 +1,12 @@
 package repositories
 
 import (
-	"database/sql"
-	"errors"
-	"fluxton/errs"
 	"fluxton/models"
 	"fluxton/utils"
-	"fmt"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
-	"github.com/lib/pq"
 	"github.com/samber/do"
+	"time"
 )
 
 type FormResponseRepository struct {
@@ -23,8 +19,25 @@ func NewFormResponseRepository(injector *do.Injector) (*FormResponseRepository, 
 	return &FormResponseRepository{db: db}, nil
 }
 
-func (r *FormResponseRepository) ListForForm(formUUID uuid.UUID) ([]models.FormField, error) {
-	query := "SELECT * FROM fluxton.form_fields WHERE form_uuid = $1;"
+func (r *FormResponseRepository) ListForForm(formUUID uuid.UUID) ([]models.FormResponse, error) {
+	query := `
+		SELECT 
+			fr.uuid AS form_response_uuid, 
+			fr.form_uuid, 
+			fr.created_at AS form_response_created_at, 
+			fr.updated_at AS form_response_updated_at, 
+			ffr.uuid AS form_field_response_uuid, 
+			ffr.form_response_uuid, 
+			ffr.form_field_uuid, 
+			ffr.value, 
+			ffr.created_at AS form_field_response_created_at, 
+			ffr.updated_at AS form_field_response_updated_at
+		FROM fluxton.form_responses fr
+		LEFT JOIN fluxton.form_field_responses ffr 
+			ON fr.uuid = ffr.form_response_uuid
+		WHERE fr.form_uuid = $1
+		ORDER BY fr.created_at, ffr.created_at;
+	`
 
 	rows, err := r.db.Queryx(query, formUUID)
 	if err != nil {
@@ -32,94 +45,72 @@ func (r *FormResponseRepository) ListForForm(formUUID uuid.UUID) ([]models.FormF
 	}
 	defer rows.Close()
 
-	var forms []models.FormField
+	formResponseMap := make(map[uuid.UUID]*models.FormResponse)
+
 	for rows.Next() {
-		var form models.FormField
-		if err := rows.StructScan(&form); err != nil {
+		var row struct {
+			FormResponseUUID           uuid.UUID  `db:"form_response_uuid"`
+			FormUUID                   uuid.UUID  `db:"form_uuid"`
+			FormResponseCreatedAt      time.Time  `db:"form_response_created_at"`
+			FormResponseUpdatedAt      time.Time  `db:"form_response_updated_at"`
+			FormFieldResponseUUID      *uuid.UUID `db:"form_field_response_uuid"`
+			FormFieldUUID              *uuid.UUID `db:"form_field_uuid"`
+			Value                      *string    `db:"value"`
+			FormFieldResponseCreatedAt *time.Time `db:"form_field_response_created_at"`
+			FormFieldResponseUpdatedAt *time.Time `db:"form_field_response_updated_at"`
+		}
+
+		if err := rows.StructScan(&row); err != nil {
 			return nil, utils.FormatError(err, "scan", utils.GetMethodName())
 		}
-		forms = append(forms, form)
-	}
 
-	if err := rows.Err(); err != nil {
-		return nil, utils.FormatError(err, "iterate", utils.GetMethodName())
-	}
-
-	return forms, nil
-}
-
-func (r *FormResponseRepository) GetByUUID(formUUID uuid.UUID) (models.FormField, error) {
-	query := "SELECT %s FROM fluxton.form_fields WHERE uuid = $1"
-	query = fmt.Sprintf(query, utils.GetColumns[models.FormField]())
-
-	var form models.FormField
-	err := r.db.Get(&form, query, formUUID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return models.FormField{}, errs.NewNotFoundError("form.error.notFound")
+		formResponse, exists := formResponseMap[row.FormResponseUUID]
+		if !exists {
+			formResponse = &models.FormResponse{
+				Uuid:      row.FormResponseUUID,
+				FormUuid:  row.FormUUID,
+				CreatedAt: row.FormResponseCreatedAt,
+				UpdatedAt: row.FormResponseUpdatedAt,
+			}
+			formResponseMap[row.FormResponseUUID] = formResponse
 		}
 
-		return models.FormField{}, utils.FormatError(err, "fetch", utils.GetMethodName())
+		if row.FormFieldResponseUUID != nil {
+			formResponse.Responses = append(formResponse.Responses, models.FormFieldResponse{
+				Uuid:             *row.FormFieldResponseUUID,
+				FormResponseUuid: row.FormResponseUUID,
+				FormFieldUuid:    *row.FormFieldUUID,
+				Value:            *row.Value,
+				CreatedAt:        *row.FormFieldResponseCreatedAt,
+				UpdatedAt:        *row.FormFieldResponseUpdatedAt,
+			})
+		}
+
+		formResponseMap[row.FormResponseUUID] = formResponse
 	}
 
-	return form, nil
-}
-
-func (r *FormResponseRepository) ExistsByUUID(formFieldUUID uuid.UUID) (bool, error) {
-	query := "SELECT EXISTS(SELECT 1 FROM fluxton.form_fields WHERE uuid = $1)"
-
-	var exists bool
-	err := r.db.Get(&exists, query, formFieldUUID)
-	if err != nil {
-		return false, utils.FormatError(err, "fetch", utils.GetMethodName())
+	formResponses := make([]models.FormResponse, 0, len(formResponseMap))
+	for _, formResponse := range formResponseMap {
+		formResponses = append(formResponses, *formResponse)
 	}
 
-	return exists, nil
+	return formResponses, nil
 }
 
-func (r *FormResponseRepository) ExistsByAnyLabelForForm(labels []string, formUUID uuid.UUID) (bool, error) {
-	query := "SELECT EXISTS(SELECT 1 FROM fluxton.form_fields WHERE label = ANY($1) AND form_uuid = $2)"
-
-	var exists bool
-	err := r.db.Get(&exists, query, pq.Array(labels), formUUID)
-	if err != nil {
-		return false, utils.FormatError(err, "fetch", utils.GetMethodName())
-	}
-
-	return exists, nil
-}
-
-func (r *FormResponseRepository) ExistsByLabelForForm(label string, formUUID uuid.UUID) (bool, error) {
-	query := "SELECT EXISTS(SELECT 1 FROM fluxton.form_fields WHERE label = $1 AND form_uuid = $2)"
-
-	var exists bool
-	err := r.db.Get(&exists, query, label, formUUID)
-	if err != nil {
-		return false, utils.FormatError(err, "fetch", utils.GetMethodName())
-	}
-
-	return exists, nil
-}
-
-func (r *FormResponseRepository) Create(formField *models.FormField) (*models.FormField, error) {
+func (r *FormResponseRepository) Create(
+	formResponse *models.FormResponse,
+	formFieldResponse *[]models.FormFieldResponse,
+) (*models.FormResponse, error) {
 	tx, err := r.db.Beginx()
 	if err != nil {
 		return nil, utils.FormatError(err, "transactionBegin", utils.GetMethodName())
 	}
 
-	query := `
-    INSERT INTO fluxton.form_fields (
-        form_uuid, label, type, description, is_required, options
-    ) VALUES (
-        $1, $2, $3, $4, $5, $6
-    )
-    RETURNING uuid
-`
+	query := `INSERT INTO fluxton.form_responses (form_uuid) VALUES ($1) RETURNING uuid`
 
 	queryErr := tx.QueryRowx(
 		query,
-		formField.FormUuid, formField.Label, formField.Type, formField.Description, formField.IsRequired, formField.Options,
-	).Scan(&formField.Uuid)
+		formResponse.FormUuid).Scan(&formResponse.Uuid)
 
 	if queryErr != nil {
 		if err := tx.Rollback(); err != nil {
@@ -128,67 +119,27 @@ func (r *FormResponseRepository) Create(formField *models.FormField) (*models.Fo
 		return nil, utils.FormatError(queryErr, "insert", utils.GetMethodName())
 	}
 
+	for _, ffr := range *formFieldResponse {
+		query = `INSERT INTO fluxton.form_field_responses (form_response_uuid, form_field_uuid, value) VALUES ($1, $2, $3) RETURNING uuid`
+		queryErr = tx.QueryRowx(
+			query,
+			formResponse.Uuid,
+			ffr.FormFieldUuid,
+			ffr.Value).Scan(&ffr.Uuid)
+		if queryErr != nil {
+			if err := tx.Rollback(); err != nil {
+				return nil, err
+			}
+			return nil, utils.FormatError(queryErr, "insert", utils.GetMethodName())
+		}
+	}
+
 	// Commit transaction
 	if err = tx.Commit(); err != nil {
 		return nil, utils.FormatError(err, "transactionCommit", utils.GetMethodName())
 	}
 
-	return formField, nil
-}
+	formResponse.Responses = *formFieldResponse
 
-func (r *FormResponseRepository) CreateMany(formFields []models.FormField, formUUID uuid.UUID) ([]models.FormField, error) {
-	createdFields := make([]models.FormField, 0, len(formFields))
-	for i, formField := range formFields {
-		formField.FormUuid = formUUID
-
-		createdField, err := r.Create(&formField)
-		if err != nil {
-			return nil, fmt.Errorf("could not create form field at index %d: %v", i, err)
-		}
-
-		utils.DumpJSON(createdField)
-		createdFields = append(createdFields, *createdField)
-	}
-
-	return createdFields, nil
-}
-
-func (r *FormResponseRepository) Update(formField *models.FormField) (*models.FormField, error) {
-	query := `
-		UPDATE fluxton.form_fields 
-		SET 
-		    label = :label, 
-		    description = :description, 
-		    type = :type, 
-		    is_required = :is_required, 
-		    options = :options, 
-		    updated_at = :updated_at
-		WHERE uuid = :uuid`
-
-	res, err := r.db.NamedExec(query, formField)
-	if err != nil {
-		return &models.FormField{}, utils.FormatError(err, "update", utils.GetMethodName())
-	}
-
-	_, err = res.RowsAffected()
-	if err != nil {
-		return &models.FormField{}, utils.FormatError(err, "affectedRows", utils.GetMethodName())
-	}
-
-	return formField, nil
-}
-
-func (r *FormResponseRepository) Delete(formFieldUUID uuid.UUID) (bool, error) {
-	query := "DELETE FROM fluxton.form_fields WHERE uuid = $1"
-	res, err := r.db.Exec(query, formFieldUUID)
-	if err != nil {
-		return false, utils.FormatError(err, "delete", utils.GetMethodName())
-	}
-
-	rowsAffected, err := res.RowsAffected()
-	if err != nil {
-		return false, utils.FormatError(err, "affectedRows", utils.GetMethodName())
-	}
-
-	return rowsAffected == 1, nil
+	return formResponse, nil
 }
