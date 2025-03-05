c38ef68a8817453859379a7fd32a2d2b33a0b5c1
Saqib Razzaq <saqib.razzaq@plana.earth>
2025-03-05 20:15:18 +0100

    add commands to be implemented later

---
+++ cmd/optimize.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+)
+
+// optimizeCmd Flush all caches and optimize the application
+var optimizeCmd = &cobra.Command{
+	Use:   "about",
+	Short: "Flush all caches and optimize the application",
+	Run: func(cmd *cobra.Command, args []string) {
+		optimize()
+	},
+}
+
+// TODO: implement later
+func optimize() {
+	// container := InitializeContainer()
+
+	log.Println("Flush all caches and optimize the application")
+}
