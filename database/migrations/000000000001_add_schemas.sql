0973f57e02778d5a062d2800f5f5c7f8c12762fb
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-07 15:22:43 +0100

    fix table permission issues

--- database/migrations/000000000001_add_schemas.sql
+++ database/migrations/000000000001_add_schemas.sql
@@ -1,5 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'web_anon') THEN
+            CREATE ROLE web_anon NOINHERIT;
+        END IF;
+    END $$;
+
 CREATE SCHEMA IF NOT EXISTS fluxton;
 CREATE SCHEMA IF NOT EXISTS authentication;
 CREATE SCHEMA IF NOT EXISTS storage;
--- database/seeders/client/02_add_roles.sql
+++ database/seeders/client/02_add_roles.sql
@@ -1,16 +1,7 @@
-CREATE ROLE web_anon NOINHERIT;
+SET ROLE fluxton;
 
--- Grant SELECT (read) on all tables
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO web_anon;
-
--- Grant USAGE on the public schema
 GRANT USAGE ON SCHEMA public TO web_anon;
-
--- Grant EXECUTE on all functions in the public schema (if needed)
 GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO web_anon;
-
--- Grant SELECT on all future tables in the public schema
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO web_anon;
-
--- Grant EXECUTE on all future functions in the public schema
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO web_anon;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO web_anon; -- future tables
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO web_anon; -- future functions
