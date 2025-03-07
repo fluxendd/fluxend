3fb8b973ff1fa7e239e15a17488f5dad3ce071b1
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-07 08:55:06 +0100

    require project header for create form request

--- requests/form_requests/create_request.go
+++ requests/form_requests/create_request.go
@@ -21,7 +21,12 @@ func (r *CreateRequest) BindAndValidate(c echo.Context) []string {
 		return []string{"Invalid request payload"}
 	}
 
-	err := validation.ValidateStruct(r,
+	err := r.WithProjectHeader(c)
+	if err != nil {
+		return []string{err.Error()}
+	}
+
+	err = validation.ValidateStruct(r,
 		validation.Field(
 			&r.Name,
 			validation.Required.Error("Name is required"),
