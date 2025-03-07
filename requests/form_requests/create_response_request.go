0678971f02533ab8af28a8fff50ce80edbbdc640
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-07 08:54:58 +0100

    require project header for create form response request

--- requests/form_requests/create_response_request.go
+++ requests/form_requests/create_response_request.go
@@ -1,16 +1,23 @@
 package form_requests
 
 import (
+	"fluxton/requests"
 	"github.com/labstack/echo/v4"
 )
 
 type CreateResponseRequest struct {
+	requests.BaseRequest
 	Response map[string]interface{} `json:"response"`
 }
 
 func (r *CreateResponseRequest) BindAndValidate(c echo.Context) []string {
 	if err := c.Bind(r); err != nil {
 		return []string{"Invalid request payload"}
+	}
+
+	err := r.WithProjectHeader(c)
+	if err != nil {
+		return []string{err.Error()}
 	}
 
 	return nil
