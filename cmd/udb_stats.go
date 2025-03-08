6232e82a0d3c3bb56ee6cf0c456c0b41363d35a6
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-08 11:20:29 +0100

    WIP: add udb stats command

--- cmd/udb_stats.go
+++ cmd/udb_stats.go
@@ -1,23 +1,50 @@
 package cmd
 
 import (
+	"fluxton/models"
 	"fluxton/services"
+	"fluxton/utils"
+	"github.com/google/uuid"
+	"github.com/samber/do"
 	"github.com/spf13/cobra"
-	"log"
 )
 
 // udbStats runs in background and populates table with stats
 var udbStats = &cobra.Command{
-	Use:   "udb.stats [project_uuid]",
-	Short: "Pull stats for a project database",
+	Use:   "udb:stats [project_uuid]",
+	Short: "Pull stats from give project's database",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		getDatabaseStats()
+		projectUUIDString := args[0]
+		getDatabaseStats(projectUUIDString)
 	},
 }
 
-// TODO: implement later
-func getDatabaseStats() {
+func getDatabaseStats(projectUUIDString string) {
 	container := InitializeContainer()
 
-	databaseStatsService := services.NewDatabaseStatsService()
+	authUser := models.AuthUser{
+		Uuid:   uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+		RoleID: 1,
+	}
+
+	projectUUID, err := uuid.Parse(projectUUIDString)
+	if err != nil {
+		utils.DumpJSON(err)
+	}
+
+	databaseStatsService := do.MustInvoke[services.DatabaseStatsService](container)
+	projectService := do.MustInvoke[services.ProjectService](container)
+
+	databaseName, err := projectService.GetDatabaseNameByUUID(projectUUID, authUser)
+	if err != nil {
+		utils.DumpJSON(err)
+	}
+
+	stats, err := databaseStatsService.GetSizePerTable(databaseName, authUser)
+	if err != nil {
+		utils.DumpJSON(err)
+	}
+
+	utils.DumpJSON(stats)
 }
