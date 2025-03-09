bb1aa5df2ca9ce261c8faf7ae12669fe0db8a70f
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-09 11:32:37 +0100

    adjust column type

--- types/table.go
+++ types/table.go
@@ -1,10 +1,11 @@
 package types
 
 type TableColumn struct {
-	Name    string
-	Type    string
-	Primary bool
-	Unique  bool
-	NotNull bool
-	Default string
+	Name     string `db:"column_name" json:"name"`
+	Position int    `db:"column_position" json:"position"`
+	NotNull  bool   `db:"not_null" json:"notNull"`
+	Type     string `db:"data_type" json:"type"`
+	Default  string `db:"default_value" json:"defaultValue"`
+	Primary  bool   `json:"primary"`
+	Unique   bool   `json:"unique"`
 }
