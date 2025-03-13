bb683168ca2a9a4d18c8840d9eb3f65f615012fe
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-13 05:35:26 +0100

    add project status constants

--- models/project.go
+++ models/project.go
@@ -5,12 +5,19 @@ import (
 	"time"
 )
 
+const ProjectStatusActive = "active"
+const ProjectStatusInactive = "inactive"
+const ProjectStatusFailed = "failed"
+const ProjectStatusFrozen = "frozen"
+
 type Project struct {
 	Uuid             uuid.UUID `db:"uuid"`
 	OrganizationUuid uuid.UUID `db:"organization_uuid"`
 	CreatedBy        uuid.UUID `db:"created_by"`
 	UpdatedBy        uuid.UUID `db:"updated_by"`
 	Name             string    `db:"name"`
+	Status           string    `db:"status"`
+	Description      string    `db:"description"`
 	DBName           string    `db:"db_name"`
 	DBPort           int       `db:"db_port"`
 	CreatedAt        time.Time `db:"created_at"`
