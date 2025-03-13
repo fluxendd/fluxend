6461d7d5fef164b7185c762cba91cf9171b78713
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-13 05:34:58 +0100

    add description column

--- database/migrations/20250131064136_add_projects_table.sql
+++ database/migrations/20250131064136_add_projects_table.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TYPE project_status AS ENUM ('active', 'inactive', 'frozen');
+CREATE TYPE project_status AS ENUM ('active', 'inactive', 'error', 'frozen');
 
 CREATE TABLE fluxton.projects (
     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -8,7 +8,8 @@ CREATE TABLE fluxton.projects (
     created_by UUID NOT NULL REFERENCES authentication.users (uuid) ON DELETE CASCADE,
     updated_by UUID NOT NULL REFERENCES authentication.users (uuid) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
-    status project_status NOT NULL DEFAULT 'active',
+    status project_status NOT NULL DEFAULT 'inactive',
+    description TEXT,
     db_name VARCHAR(255) NOT NULL,
     db_port INT NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
