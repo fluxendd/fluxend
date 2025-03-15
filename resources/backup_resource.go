78d4d6e2a5ea69d3afd1e9d8e31a6a41d95286e6
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-03-15 11:06:39 +0100

    FLXX-62: fix response issue

--- resources/backup_resource.go
+++ resources/backup_resource.go
@@ -15,13 +15,18 @@ type BackupResponse struct {
 }
 
 func BackupResource(backup *models.Backup) BackupResponse {
+	completedAt := ""
+	if backup.CompletedAt != nil {
+		completedAt = backup.CompletedAt.Format("2006-01-02 15:04:05")
+	}
+
 	return BackupResponse{
 		Uuid:        backup.Uuid,
 		ProjectUuid: backup.ProjectUuid,
 		Status:      backup.Status,
 		Error:       backup.Error,
 		StartedAt:   backup.StartedAt.Format("2006-01-02 15:04:05"),
-		CompletedAt: backup.CompletedAt.Format("2006-01-02 15:04:05"),
+		CompletedAt: completedAt,
 	}
 }
 
