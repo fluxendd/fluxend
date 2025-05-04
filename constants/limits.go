62e6e7728c72ec3ee50164bb503713787f7d1080
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-04 09:31:52 +0200

    define limits

---
+++ constants/limits.go
@@ -0,0 +1,24 @@
+package constants
+
+const (
+	MaxTableNameLength            = 60
+	MinTableNameLength            = 3
+	MaxColumnNameLength           = 60
+	MinColumnNameLength           = 2
+	MaxIndexNameLength            = 60
+	MinIndexNameLength            = 3
+	MaxOrganizationNameLength     = 100
+	MinOrganizationNameLength     = 3
+	MaxProjectNameLength          = 100
+	MinProjectNameLength          = 3
+	MinFormFieldLabelLength       = 3
+	MaxFormFieldLabelLength       = 100
+	MinFormFieldDescriptionLength = 0
+	MaxFormFieldDescriptionLength = 255
+	MinBucketNameLength           = 3
+	MaxBucketNameLength           = 63
+	MinBucketDescriptionLength    = 0
+	MaxBucketDescriptionLength    = 255
+	MinFileNameLength             = 3
+	MaxFileNameLength             = 63
+)
