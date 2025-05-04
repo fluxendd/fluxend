328f39db92c3f4da453c3de2b485174c87310341
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-04 09:29:28 +0200

    add action strings, refactor message

---
+++ constants/action.go
@@ -0,0 +1,11 @@
+package configs
+
+const (
+	ActionAPIRequest = "api_request"
+
+	ActionClientDatabaseCreate  = "client_database_create"
+	ActionClientDatabaseConnect = "client_database_connect"
+	ActionClientDatabaseSeed    = "client_database_seed"
+
+	ActionPostgrest = "postgrest"
+)
