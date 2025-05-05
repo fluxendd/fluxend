05dbb51f26224bc8af8b93dd5a054e770a144bc1
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-05 19:25:02 +0200

    Add status column to api_logs table

--- database/migrations/20250305085430_add_api_logs_table.sql
+++ database/migrations/20250305085430_add_api_logs_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE fluxton.request_logs (
     user_uuid UUID NULL,
     api_key UUID NULL,
     method VARCHAR(10) NOT NULL,
+    status INTEGER NOT NULL,
     endpoint TEXT NOT NULL,
     ip_address INET NOT NULL,
     user_agent VARCHAR(255) NULL,
