9e213d8848ac7683c82c1ddc2c8a7703d2e1c6b3
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-06 17:21:07 +0200

    Add dropbox storage engine constant

--- constants/general.go
+++ constants/general.go
@@ -4,5 +4,6 @@ const (
 	BackupBucketName        = "fluxton-client-database-backups"
 	StorageEngineFilesystem = "FILESYSTEM"
 	StorageEngineS3         = "S3"
+	StorageEngineDropbox    = "DROPBOX"
 	StorageEngineBackBlaze  = "BACKBLAZE"
 )
