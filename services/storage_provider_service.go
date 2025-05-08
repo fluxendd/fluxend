26f856ab06e3df122276528a65a8f1ead7c6d54d
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-08 11:45:11 +0200

    Trim service to get storage provider

--- services/storage_provider_service.go
+++ services/storage_provider_service.go
@@ -4,17 +4,7 @@ import (
 	"fmt"
 )
 
-type StorageProviderService interface {
-	GetProvider(provider string) (StorageService, error)
-}
-
-type StorageProviderServiceImpl struct{}
-
-func NewStorageProviderService() (StorageProviderService, error) {
-	return &StorageProviderServiceImpl{}, nil
-}
-
-func (s *StorageProviderServiceImpl) GetProvider(provider string) (StorageService, error) {
+func GetStorageProvider(provider string) (StorageService, error) {
 	switch provider {
 	case "s3":
 		return NewS3Service()
