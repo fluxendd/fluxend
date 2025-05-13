ebd97392b5b05d30f4131ff3993f99fc2f9348b4
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-13 07:00:54 +0200

    Require ctx in Send for access to settings

--- services/email/interface.go
+++ services/email/interface.go
@@ -1,5 +1,9 @@
 package email
 
+import (
+	"github.com/labstack/echo/v4"
+)
+
 type EmailInterface interface {
-	Send(to, subject, body string) error
+	Send(ctx echo.Context, to, subject, body string) error
 }
