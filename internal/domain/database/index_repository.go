2101e033b16299a0d660ed898d85e7e35b445bcb
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-19 07:59:23 +0200

    Define repo interfaces

--- internal/domain/database/index_repository.go
+++ internal/domain/database/index_repository.go
@@ -1,4 +1,9 @@
 package database
 
 type IndexRepository interface {
+	GetByName(tableName, indexName string) (string, error)
+	Has(tableName, indexName string) (bool, error)
+	List(tableName string) ([]string, error)
+	Create(tableName string, indexName string, columns []string, isUnique bool) (bool, error)
+	DropIfExists(indexName string) (bool, error)
 }
