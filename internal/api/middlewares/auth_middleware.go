0729c5e325c58ba5d6d80949efc141c1fa35c906
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-21 06:50:34 +0200

    Fix: allow multiple login sessions

--- internal/api/middlewares/auth_middleware.go
+++ internal/api/middlewares/auth_middleware.go
@@ -51,13 +51,14 @@ func AuthMiddleware(userRepo user.Repository) echo.MiddlewareFunc {
 				return response.ErrorResponse(c, errors.NewUnauthorizedError("auth.error.tokenInvalid"))
 			}
 
-			currentJWTVersion := int(claims["version"].(float64))
-			existingJWTVersion, err := userRepo.GetJWTVersion(userUUID)
+			loggedInJWTVersion := int(claims["version"].(float64))
+			latestVersion, err := userRepo.GetJWTVersion(userUUID)
 			if err != nil {
 				return response.ErrorResponse(c, err)
 			}
 
-			if currentJWTVersion != existingJWTVersion {
+			// Allow a max 5 sessions to be active at the same time
+			if (latestVersion - loggedInJWTVersion) >= 5 {
 				return response.UnauthorizedResponse(c, "auth.error.tokenInvalid")
 			}
 
