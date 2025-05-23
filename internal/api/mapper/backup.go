465035f2a8ba2425938e3e79632929466553c118
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-05-23 08:11:54 +0200

    FLX-56: Move backup mapper out of dir

--- internal/api/mapper/backup.go
+++ internal/api/mapper/backup.go
@@ -1,11 +1,11 @@
-package backup
+package mapper
 
 import (
 	backupDto "fluxton/internal/api/dto/backup"
 	"fluxton/internal/domain/backup"
 )
 
-func ToResource(backup *backup.Backup) backupDto.Response {
+func ToBackupResource(backup *backup.Backup) backupDto.Response {
 	completedAt := ""
 	if backup.CompletedAt != nil {
 		completedAt = backup.CompletedAt.Format("2006-01-02 15:04:05")
@@ -21,10 +21,10 @@ func ToResource(backup *backup.Backup) backupDto.Response {
 	}
 }
 
-func ToResourceCollection(backups []backup.Backup) []backupDto.Response {
+func ToBackupResourceCollection(backups []backup.Backup) []backupDto.Response {
 	resourceBackups := make([]backupDto.Response, len(backups))
 	for i, currentBackup := range backups {
-		resourceBackups[i] = ToResource(&currentBackup)
+		resourceBackups[i] = ToBackupResource(&currentBackup)
 	}
 
 	return resourceBackups
