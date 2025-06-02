cf96c81058cea68d5fcccd21a34cf22be86ed67d
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-06-02 11:30:19 +0200

    FLX-18: Simplify & refactor user repo

--- internal/database/repositories/user.go
+++ internal/database/repositories/user.go
@@ -1,8 +1,6 @@
 package repositories
 
 import (
-	"database/sql"
-	"errors"
 	"fluxend/internal/config/constants"
 	"fluxend/internal/domain/shared"
 	"fluxend/internal/domain/user"
@@ -11,17 +9,16 @@ import (
 	flxErrs "fluxend/pkg/errors"
 	"fmt"
 	"github.com/google/uuid"
-	"github.com/jmoiron/sqlx"
 	"github.com/samber/do"
 	"time"
 )
 
 type UserRepository struct {
-	db *sqlx.DB
+	db shared.DB
 }
 
 func NewUserRepository(injector *do.Injector) (user.Repository, error) {
-	db := do.MustInvoke[*sqlx.DB](injector)
+	db := do.MustInvoke[shared.DB](injector)
 	return &UserRepository{db: db}, nil
 }
 
@@ -39,94 +36,40 @@ func (r *UserRepository) List(paginationParams shared.PaginationParams) ([]user.
 		"offset": offset,
 	}
 
-	rows, err := r.db.NamedQuery(query, params)
-	if err != nil {
-		return nil, pkg.FormatError(err, "select", pkg.GetMethodName())
-	}
-	defer rows.Close()
-
 	var users []user.User
-	for rows.Next() {
-		var currentUser user.User
-		if err := rows.StructScan(&currentUser); err != nil {
-			return nil, pkg.FormatError(err, "scan", pkg.GetMethodName())
-		}
-		users = append(users, currentUser)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, pkg.FormatError(err, "iterate", pkg.GetMethodName())
-	}
-
-	return users, nil
+	return users, r.db.SelectNamedList(&users, query, params)
 }
 
 func (r *UserRepository) GetByID(userUUID uuid.UUID) (user.User, error) {
 	query := fmt.Sprintf("SELECT %s FROM authentication.users WHERE uuid = $1", pkg.GetColumns[user.User]())
+
 	var fetchedUser user.User
-	err := r.db.Get(&fetchedUser, query, userUUID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return user.User{}, flxErrs.NewNotFoundError("user.error.notFound")
-		}
-
-		return user.User{}, pkg.FormatError(err, "fetch", pkg.GetMethodName())
-	}
-
-	return fetchedUser, nil
+	return fetchedUser, r.db.GetWithNotFound(&fetchedUser, "user.error.notFound", query, userUUID)
 }
 
 func (r *UserRepository) ExistsByID(userUUID uuid.UUID) (bool, error) {
-	query := "SELECT EXISTS(SELECT 1 FROM authentication.users WHERE uuid = $1)"
-	var exists bool
-	err := r.db.Get(&exists, query, userUUID)
-	if err != nil {
-		return false, pkg.FormatError(err, "fetch", pkg.GetMethodName())
-	}
-
-	return exists, nil
+	return r.db.Exists("authentication.users", "uuid = $1", userUUID)
 }
 
 func (r *UserRepository) ExistsByEmail(email string) (bool, error) {
-	query := "SELECT EXISTS(SELECT 1 FROM authentication.users WHERE email = $1)"
-	var exists bool
-	err := r.db.Get(&exists, query, email)
-	if err != nil {
-		return false, pkg.FormatError(err, "fetch", pkg.GetMethodName())
-	}
-
-	return exists, nil
+	return r.db.Exists("authentication.users", "email = $1", email)
 }
 
 func (r *UserRepository) ExistsByUsername(username string) (bool, error) {
-	query := "SELECT EXISTS(SELECT 1 FROM authentication.users WHERE username = $1)"
-	var exists bool
-	err := r.db.Get(&exists, query, username)
-	if err != nil {
-		return false, pkg.FormatError(err, "fetch", pkg.GetMethodName())
-	}
-
-	return exists, nil
+	return r.db.Exists("authentication.users", "username = $1", username)
 }
 
 func (r *UserRepository) GetByEmail(email string) (user.User, error) {
 	query := fmt.Sprintf("SELECT %s FROM authentication.users WHERE email = $1", pkg.GetColumns[user.User]())
+
 	var fetchedUser user.User
-	err := r.db.Get(&fetchedUser, query, email)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return user.User{}, flxErrs.NewNotFoundError("user.error.notFound")
-		}
-
-		return user.User{}, fmt.Errorf("could not fetch rowx: %v", err)
-	}
-
-	return fetchedUser, nil
+	return fetchedUser, r.db.GetWithNotFound(&fetchedUser, "user.error.notFound", query, email)
 }
 
 func (r *UserRepository) Create(input *user.User) (*user.User, error) {
 	query := "INSERT INTO authentication.users (username, email, status, role_id, password) VALUES ($1, $2, $3, $4, $5) RETURNING uuid"
-	err := r.db.QueryRowx(query, input.Username, input.Email, constants.UserStatusActive, input.RoleID, auth.HashPassword(input.Password)).Scan(&input.Uuid)
+
+	err := r.db.QueryRow(query, input.Username, input.Email, constants.UserStatusActive, input.RoleID, auth.HashPassword(input.Password)).Scan(&input.Uuid)
 	if err != nil {
 		return &user.User{}, fmt.Errorf("could not create row: %v", err)
 	}
@@ -157,12 +100,13 @@ func (r *UserRepository) CreateJWTVersion(userId uuid.UUID) (int, error) {
 func (r *UserRepository) GetJWTVersion(userId uuid.UUID) (int, error) {
 	query := "SELECT version FROM authentication.jwt_versions WHERE user_id = $1"
 	var version int
+
 	err := r.db.Get(&version, query, userId)
 	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
+		// Check if it's a not found error and return custom error
+		if err.Error() == "sql: no rows in result set" {
 			return 0, flxErrs.NewUnauthorizedError("auth.error.tokenExpired")
 		}
-
 		return 0, fmt.Errorf("could not fetch JWT version: %v", err)
 	}
 
@@ -178,30 +122,14 @@ func (r *UserRepository) Update(userUUID uuid.UUID, inputUser *user.User) (*user
 		SET bio = :bio, updated_at = :updated_at 
 		WHERE uuid = :uuid`
 
-	res, err := r.db.NamedExec(query, inputUser)
-	if err != nil {
-		return &user.User{}, pkg.FormatError(err, "update", pkg.GetMethodName())
-	}
-
-	_, err = res.RowsAffected()
-	if err != nil {
-		return &user.User{}, pkg.FormatError(err, "affectedRows", pkg.GetMethodName())
-	}
-
-	return inputUser, nil
+	_, err := r.db.NamedExecWithRowsAffected(query, inputUser)
+	return inputUser, err
 }
 
 func (r *UserRepository) Delete(userUUID uuid.UUID) (bool, error) {
-	query := "DELETE FROM authentication.users WHERE uuid = $1"
-	res, err := r.db.Exec(query, userUUID)
+	rowsAffected, err := r.db.ExecWithRowsAffected("DELETE FROM authentication.users WHERE uuid = $1", userUUID)
 	if err != nil {
-		return false, pkg.FormatError(err, "delete", pkg.GetMethodName())
+		return false, err
 	}
-
-	rowsAffected, err := res.RowsAffected()
-	if err != nil {
-		return false, pkg.FormatError(err, "affectedRows", pkg.GetMethodName())
-	}
-
 	return rowsAffected == 1, nil
 }
