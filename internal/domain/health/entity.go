46a69b96109fb335a6213f1ddc6d7dba293eb277
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-06-03 07:33:26 +0200

    FLX-113: Extend Health entity

--- internal/domain/health/entity.go
+++ internal/domain/health/entity.go
@@ -1,7 +1,18 @@
 package health
 
 type Health struct {
+	// Service Status
 	DatabaseStatus  string `json:"database_status"`
 	AppStatus       string `json:"app_status"`
 	PostgrestStatus string `json:"postgrest_status"`
+
+	// System Resources
+	RamUsage      string `json:"ram_usage"`
+	RamAvailable  string `json:"ram_available"`
+	RamTotal      string `json:"ram_total"`
+	DiskUsage     string `json:"disk_usage"`
+	DiskAvailable string `json:"disk_available"`
+	DiskTotal     string `json:"disk_total"`
+	CPUUsage      string `json:"cpu_usage"`
+	CPUCores      int    `json:"cpu_cores"`
 }
