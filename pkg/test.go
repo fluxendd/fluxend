4567023b30afce7827b2c74d98a0cb2a36141919
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-06-09 09:40:39 +0200

    FLX-61: Remove target param

--- pkg/test.go
+++ pkg/test.go
@@ -10,11 +10,12 @@ import (
 	"testing"
 )
 
-func CreateTestContext(t *testing.T, e *echo.Echo, method, target string, payload map[string]interface{}) echo.Context {
+func CreateFakeRequestContext(t *testing.T, e *echo.Echo, method string, payload map[string]interface{}) echo.Context {
 	body, err := json.Marshal(payload)
 	assert.NoError(t, err, "Failed to marshal payload")
 
-	fakeRequest := httptest.NewRequest(method, target, bytes.NewReader(body))
+	// These requests are bound and validated directly. So target doesn't matter.
+	fakeRequest := httptest.NewRequest(method, "/", bytes.NewReader(body))
 	fakeRequest.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 
 	return e.NewContext(fakeRequest, httptest.NewRecorder())
