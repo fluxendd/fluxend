13535dc41509ffb8df4f4a669074292b223d5ec7
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-07-02 16:09:52 +0200

    Minor adjustments

--- internal/api/handlers/log.go
+++ internal/api/handlers/log.go
@@ -45,7 +45,7 @@ func NewLogHandler(injector *do.Injector) (*LogHandler, error) {
 // @Param sort query string false "Field to sort by"
 // @Param order query string false "Sort order (asc or desc)"
 //
-// @Success 200 {array} response.Response{content=[]logging.Response} "List of files"
+// @Success 200 {array} response.Response{content=[]logging.Response} "List of logs"
 // @Failure 400 {object} response.BadRequestErrorResponse "Bad request response"
 // @Failure 401 {object} response.UnauthorizedErrorResponse "Unauthorized response"
 // @Failure 500 {object} response.InternalServerErrorResponse "Internal server error response"
--- internal/api/response/success_with_pagination.go
+++ internal/api/response/success_with_pagination.go
@@ -8,14 +8,10 @@ import (
 
 func SuccessResponseWithPagination(c echo.Context, content interface{}, paginationDetails shared.PaginationDetails) error {
 	response := Response{
-		Success: true,
-		Errors:  nil,
-		Content: content,
-		Metadata: map[string]interface{}{
-			"total": paginationDetails.Total,
-			"page":  paginationDetails.Page,
-			"limit": paginationDetails.Limit,
-		},
+		Success:  true,
+		Errors:   nil,
+		Content:  content,
+		Metadata: paginationDetails,
 	}
 
 	return c.JSON(http.StatusOK, response)
