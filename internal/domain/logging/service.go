5ae3bb8a4e5b703055690c4f70ba847521336425
Saqib Razzaq <saqibrzzaq@gmail.com>
2025-07-12 17:51:12 +0200

    Fix permission check for logs

--- internal/domain/logging/service.go
+++ internal/domain/logging/service.go
@@ -1,8 +1,8 @@
 package logging
 
 import (
-	"fluxend/internal/domain/admin"
 	"fluxend/internal/domain/auth"
+	"fluxend/internal/domain/project"
 	"fluxend/internal/domain/shared"
 	"fluxend/pkg/errors"
 	"github.com/samber/do"
@@ -17,23 +17,31 @@ type Service interface {
 }
 
 type ServiceImpl struct {
-	adminPolicy *admin.Policy
-	logRepo     Repository
+	projectPolicy *project.Policy
+	logRepo       Repository
+	projectRepo   project.Repository
 }
 
 func NewLogService(injector *do.Injector) (Service, error) {
-	policy := admin.NewAdminPolicy()
+	policy := do.MustInvoke[*project.Policy](injector)
 	logRepo := do.MustInvoke[Repository](injector)
+	projectRepo := do.MustInvoke[project.Repository](injector)
 
 	return &ServiceImpl{
-		adminPolicy: policy,
-		logRepo:     logRepo,
+		projectPolicy: policy,
+		logRepo:       logRepo,
+		projectRepo:   projectRepo,
 	}, nil
 }
 
 func (s *ServiceImpl) List(listInput *ListInput, paginationParams shared.PaginationParams, authUser auth.User) ([]RequestLog, shared.PaginationDetails, error) {
-	if !s.adminPolicy.CanAccess(authUser) {
-		return []RequestLog{}, shared.PaginationDetails{}, errors.NewForbiddenError("log.error.listForbidden")
+	fetchedProject, err := s.projectRepo.GetByUUID(listInput.ProjectUuid.UUID)
+	if err != nil {
+		return nil, shared.PaginationDetails{}, err
+	}
+
+	if !s.projectPolicy.CanAccess(fetchedProject.OrganizationUuid, authUser) {
+		return nil, shared.PaginationDetails{}, errors.NewForbiddenError("project.error.viewForbidden")
 	}
 
 	return s.logRepo.List(listInput, paginationParams)
